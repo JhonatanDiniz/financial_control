99eb476e5de8475a3ccd356638f60aaa0880583e
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2023-11-08 15:00:39 -0300

    Configured H2 data base

---
+++ src/main/java/com/finance/personalcontrol/PersonalControlApplication.java
@@ -0,0 +1,13 @@
+package com.finance.personalcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalControlApplication.class, args);
+	}
+
+}
